d4c61fd4209dd38da830c7615facf462799bd82d
smitchhatrola2505 <schhatrola779@rku.ac.in>
2023-03-03 18:29:43 +0530

    03/03/2023

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 
+// var gulp = require("gulp");
+
+// gulp.task("Copy-dist-to-wwwroot", () => {
+//     return gulp.src("./dist/TaskManager/**/*")
+//         .pipe(gulp.dest("C:\\Training\\Angular\\MvcTaskManager\\MvcTaskManager\\wwwroot"));
+// });
+
+// gulp.task("default", () => {
+//     gulp.watch("./dist/TaskManager", gulp.series("Copy-dist-to-wwwroot"));
+// });
+
 var gulp = require("gulp");
-
-gulp.task("Copy-dist-to-wwwroot", () => {
-    return gulp.src("./dist/TaskManager/**/*")
-        .pipe(gulp.dest("C:\\Training\\Angular\\MvcTaskManager\\MvcTaskManager\\wwwroot"));
+ gulp.task("Copy-dist-to-wwwroot", () => { 
+    return gulp.src("./dist/task-manager/**/*")
+    .pipe(gulp.dest("C:\\Training\\Angular\\MvcTaskManager\\MvcTaskManager\\wwwroot")); 
 });
-
-gulp.task("default", () => {
-    gulp.watch("./dist/TaskManager", gulp.series("Copy-dist-to-wwwroot"));
-});
+     gulp.task("default", () => { 
+        gulp.watch("./dist/task-manager", gulp.series("Copy-dist-to-wwwroot")); 
+    })
